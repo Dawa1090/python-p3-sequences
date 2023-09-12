d68c7f83887d1481b86d34d820b5ce2ce2e943ad
Dawa Sherpa <dawasherpa1090@gmail.com>
2023-09-12 19:22:25 -0400

    p3 sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    if length == 0:
+        print(fibonacci_sequence)
+        return
+
+    a, b = 0, 1
+    
+    fibonacci_sequence.append(a)
+    for _ in range(length - 1):
+        a, b = b, a + b  
+        fibonacci_sequence.append(a)
+    
+    print(fibonacci_sequence)
+    
+    
